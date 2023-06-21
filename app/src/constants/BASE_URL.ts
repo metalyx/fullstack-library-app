58f5a2c39ba186315e11ae815301abbd24f2b285
Vitalii <metalyx3@gmail.com>
2023-06-21 14:11:45 -0700

    change base url in frontend

--- app/src/constants/BASE_URL.ts
+++ app/src/constants/BASE_URL.ts
@@ -1,5 +1,5 @@
 export const BASE_URL =
     process.env.NODE_ENV === 'development'
         ? 'http://localhost:5000/'
-        : 'https://fullstack-library-app.onrender.com/';
+        : 'https://fullstack-library-app-ij27.onrender.com/';
 // : 'https://express-librarian.herokuapp.com/'; // Heroku old backend location
